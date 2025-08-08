25207a211e9e9d8a9e7ce9883fb318d9130c4e48
Bradley Adams <bradley.wayne.adams@gmail.com>
2025-08-08 13:17:35 +1200

    adding a theme using m3

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -9,19 +9,10 @@ import { HeaderComponent } from './components/header/header.component'
     standalone: true,
     imports: [CommonModule, RouterOutlet, MatSidenavModule, HeaderComponent],
     template: `
-      <app-header></app-header>
-      <router-outlet></router-outlet>
+        <div class="mat-typography app-frame mat-app-background">
+            <app-header></app-header>
+            <router-outlet></router-outlet>
+        </div>
     `,
-    styles: `
-        .contents {
-            box-sizing: border-box;
-            background-color: transparent;
-            margin: 0;
-            position: absolute;
-            height: auto;
-            inset: 0;
-            padding: 0;
-        }
-    `
 })
 export class AppComponent {}
--- ui/src/app/pages/schedule/schedule.component.spec.ts
+++ ui/src/app/pages/schedule/schedule.component.spec.ts
@@ -2,13 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleComponent } from './schedule.component';
 
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+
 describe('ScheduleComponent', () => {
   let component: ScheduleComponent;
   let fixture: ComponentFixture<ScheduleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ScheduleComponent]
+      imports: [ScheduleComponent, NoopAnimationsModule],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting() 
+      ]
     })
     .compileComponents();
     
