dcdf8e75eb9a8e8f8ee20e8b52d32d8710e17f49
Bradley Adams <bradley.wayne.adams@gmail.com>
2025-08-13 08:21:57 +1200

    adding admin tools icon to header

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing'
+import { AppComponent } from './app.component'
+import { provideHttpClient } from '@angular/common/http'
+import { provideHttpClientTesting } from '@angular/common/http/testing'
+import { provideRouter } from '@angular/router'
+import { CompetitionComponent } from './pages/competition/competition.component'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent],
+            providers: [
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                provideRouter([{ path: 'competitions/create', component: CompetitionComponent }])
+            ]
+        }).compileComponents()
+    })
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-});
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent)
+        const app = fixture.componentInstance
+        expect(app).toBeTruthy()
+    })
+
+    it('should render the header component', () => {
+        const fixture = TestBed.createComponent(AppComponent)
+        fixture.detectChanges()
+        const headerEl = fixture.nativeElement.querySelector('app-header')
+        expect(headerEl).toBeTruthy()
+    })
+
+    it('should include a router outlet', () => {
+        const fixture = TestBed.createComponent(AppComponent)
+        fixture.detectChanges()
+        const outlet = fixture.nativeElement.querySelector('router-outlet')
+        expect(outlet).toBeTruthy()
+    })
+})
--- ui/src/app/components/header/header.component.ts
+++ ui/src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component} from '@angular/core'
+import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import {MatToolbarModule} from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, RouterModule, MatToolbarModule],
+    imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss']
 })
