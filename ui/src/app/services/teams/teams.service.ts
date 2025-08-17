e8e39b15eda0d45475beac9de2cbfffdbae24475
Bradley Adams <bradley.wayne.adams@gmail.com>
2025-08-18 09:56:02 +1200

    fix team components

--- ui/src/app/services/teams/teams.service.ts
+++ ui/src/app/services/teams/teams.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Team } from '../../types/api'
 import { environment } from '../../../environments/environment'
+import { Observable } from 'rxjs'
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +11,23 @@ export class TeamsService {
     private readonly path = environment.apiUrl
     private readonly http = inject(HttpClient)
 
-    getTeams() {
+    getTeams(): Observable<Team[]> {
         return this.http.get<Team[]>(`${this.path}/v1/teams `)
+    }
+
+    getTeam(id: string): Observable<Team> {
+        return this.http.get<Team>(`${this.path}/v1/teams/${id}`)
+    }
+
+    createTeam(team: Partial<Team>): Observable<Team> {
+        return this.http.post<Team>(`${this.path}/v1/teams`, team)
+    }
+
+    updateTeam(id: string, team: Partial<Team>): Observable<Team> {
+        return this.http.put<Team>(`${this.path}/v1/teams/${id}`, team)
+    }
+
+    deleteTeam(id: string): Observable<void> {
+        return this.http.delete<void>(`${this.path}/v1/teams/${id}`)
     }
 }
