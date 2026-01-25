a7f68b95958df43b35875d78f982a24165a4ea38
bradley-adams <bradley.wayne.adams@gmail.com>
2026-01-26 07:37:13 +1300

    adding VerifyMigrations

--- db/connection.go
+++ db/connection.go
@@ -2,9 +2,20 @@ package db
 
 import (
 	"database/sql"
+	"embed"
 	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+
+	_ "github.com/lib/pq"
 )
 
+//go:embed migrations/*.sql
+var migrationFiles embed.FS
+
+// Open opens a database connection and pings it.
 func Open(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
@@ -17,4 +28,36 @@ func Open(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+// VerifyMigrations checks that all embedded migrations have been applied and that the schema is not dirty.
+func VerifyMigrations(db *sql.DB) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("migrate driver: %w", err)
+	}
+
+	sourceDriver, err := iofs.New(migrationFiles, "migrations")
+	if err != nil {
+		return fmt.Errorf("migrate source: %w", err)
+	}
+
+	m, err := migrate.NewWithInstance(
+		"iofs", sourceDriver,
+		"postgres", driver,
+	)
+	if err != nil {
+		return fmt.Errorf("migrate init: %w", err)
+	}
+
+	version, dirty, err := m.Version()
+	if err != nil && err != migrate.ErrNilVersion {
+		return fmt.Errorf("migration version check failed: %w", err)
+	}
+
+	if dirty {
+		return fmt.Errorf("database schema is dirty at version %d", version)
+	}
+
+	return nil
 }
