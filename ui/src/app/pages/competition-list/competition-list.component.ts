b86113bd30571a010f166d7903b7c621923a7c32
bradley-adams <bradley.wayne.adams@gmail.com>
2026-03-02 06:17:23 +1300

    adding pagination funcaitonality to ts

--- ui/src/app/pages/competition-list/competition-list.component.ts
+++ ui/src/app/pages/competition-list/competition-list.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common'
 import { Component, inject } from '@angular/core'
+import { MatPaginator, PageEvent } from '@angular/material/paginator'
 import { MatTableDataSource } from '@angular/material/table'
 import { RouterModule } from '@angular/router'
 import { BreadcrumbComponent } from '../../components/breadcrumb/breadcrumb.component'
@@ -11,7 +12,7 @@ import { Competition } from '../../types/api'
 @Component({
     selector: 'app-competition-list',
     standalone: true,
-    imports: [CommonModule, MaterialModule, RouterModule, BreadcrumbComponent],
+    imports: [CommonModule, MaterialModule, RouterModule, BreadcrumbComponent, MatPaginator],
     templateUrl: './competition-list.component.html',
     styleUrl: './competition-list.component.scss'
 })
@@ -20,15 +21,25 @@ export class CompetitionListComponent {
     private readonly notificationService = inject(NotificationService)
 
     public dataSource = new MatTableDataSource<Competition>([])
+    public total = 0
+    public page = 1
+    public pageSize = 10
 
     ngOnInit(): void {
         this.loadCompetitions()
     }
 
+    public onPageChange(event: PageEvent): void {
+        this.page = event.pageIndex + 1
+        this.pageSize = event.pageSize
+        this.loadCompetitions()
+    }
+
     private loadCompetitions(): void {
-        this.competitionsService.getCompetitions().subscribe({
-            next: (competitions) => {
-                this.dataSource.data = competitions
+        this.competitionsService.getPaginatedCompetitions(this.page, this.pageSize).subscribe({
+            next: (response) => {
+                this.dataSource.data = response.data
+                this.total = response.pagination.total
             },
             error: (err) => {
                 this.notificationService.showErrorAndLog('Load Error', 'Failed to load competitions', err)
