44cc255283683109bddd0c04bb252481b28294b2
bradley-adams <bradley.wayne.adams@gmail.com>
2026-04-30 06:35:57 +1200

    adding validation on pagination

--- api/http/api/pagination.go
+++ api/http/api/pagination.go
@@ -1,5 +1,10 @@
 package api
 
+type PaginationRequest struct {
+	Page     int `form:"page" validate:"omitempty,gte=1"`
+	PageSize int `form:"page_size" validate:"omitempty,gte=1,lte=100"`
+}
+
 type PaginationMeta struct {
 	Page       int   `json:"page"`
 	PageSize   int   `json:"page_size"`
@@ -10,4 +15,17 @@ type PaginationMeta struct {
 type PaginatedResponse[T any] struct {
 	Data       []T            `json:"data"`
 	Pagination PaginationMeta `json:"pagination"`
+}
+
+func (q *PaginationRequest) SetDefaults() {
+	if q.Page == 0 {
+		q.Page = 1
+	}
+	if q.PageSize == 0 {
+		q.PageSize = 20
+	}
+}
+
+func (q *PaginationRequest) Offset() int {
+	return (q.Page - 1) * q.PageSize
 }
