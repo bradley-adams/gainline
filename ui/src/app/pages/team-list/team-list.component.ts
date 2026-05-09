1ec2f888bf424da84be63e749e7a07524ef21af8
bradley-adams <bradley.wayne.adams@gmail.com>
2026-05-10 09:28:33 +1200

    Adding pagination to team list

--- ui/src/app/pages/team-list/team-list.component.ts
+++ ui/src/app/pages/team-list/team-list.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core'
-import { MatTableDataSource } from '@angular/material/table'
-import { Team } from '../../types/api'
 import { CommonModule } from '@angular/common'
-import { MaterialModule } from '../../shared/material/material.module'
+import { Component, inject } from '@angular/core'
+import { MatPaginator, PageEvent } from '@angular/material/paginator'
+import { MatTableDataSource } from '@angular/material/table'
 import { RouterModule } from '@angular/router'
-import { TeamsService } from '../../services/teams/teams.service'
 import { NotificationService } from '../../services/notifications/notifications.service'
+import { TeamsService } from '../../services/teams/teams.service'
+import { MaterialModule } from '../../shared/material/material.module'
+import { Team } from '../../types/api'
 
 @Component({
     selector: 'app-team-list',
     standalone: true,
-    imports: [CommonModule, MaterialModule, RouterModule],
+    imports: [CommonModule, MaterialModule, RouterModule, MatPaginator],
     templateUrl: './team-list.component.html',
     styleUrl: './team-list.component.scss'
 })
@@ -20,14 +21,25 @@ export class TeamListComponent {
 
     public dataSource = new MatTableDataSource<Team>([])
 
+    public total = 0
+    public page = 1
+    public pageSize = 10
+
     ngOnInit(): void {
         this.loadTeams()
     }
 
+    public onPageChange(event: PageEvent): void {
+        this.page = event.pageIndex + 1
+        this.pageSize = event.pageSize
+        this.loadTeams()
+    }
+
     private loadTeams(): void {
-        this.teamsService.getTeams().subscribe({
-            next: (teams) => {
-                this.dataSource.data = teams
+        this.teamsService.getTeamsPaginated(this.page, this.pageSize).subscribe({
+            next: (response) => {
+                this.dataSource.data = response.data
+                this.total = response.pagination.total
             },
             error: (err) => {
                 this.notificationService.showErrorAndLog('Load Error', 'Failed to load teams', err)
