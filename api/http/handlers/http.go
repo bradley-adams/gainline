ea24ecd90c1c66a8968f1b6eec5f2709c79b0b8f
Bradley Adams <bradley.wayne.adams@gmail.com>
2026-06-26 06:56:33 +1200

    refactor SetupRouter to use RouterConfig struct

--- api/http/handlers/http.go
+++ api/http/handlers/http.go
@@ -16,18 +16,25 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+type RouterConfig struct {
+	DB              db_handler.DB
+	Logger          zerolog.Logger
+	Validate        *validator.Validate
+	GameStateClient *gamestate.Client
+}
+
 // SetupRouter initializes and configures the HTTP router for handling incoming requests
 //
 //	@title			Gainline Api
 //	@description	A set of endpoints for managing gainline tasks
 //	@version		1.0
-func SetupRouter(db db_handler.DB, logger zerolog.Logger, validate *validator.Validate, gameStateClient *gamestate.Client) *gin.Engine {
-	logger.Debug().Msg("setting up http router...")
+func SetupRouter(cfg RouterConfig) *gin.Engine {
+	cfg.Logger.Debug().Msg("setting up http router...")
 
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 
-	router.GET("/health", healthCheck(db, logger))
+	router.GET("/health", healthCheck(cfg.DB, cfg.Logger))
 
 	docs.SwaggerInfo.BasePath = "/v1"
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
@@ -37,41 +44,41 @@ func SetupRouter(db db_handler.DB, logger zerolog.Logger, validate *validator.Va
 		v1public.OPTIONS("/*path")
 
 		// middleware
-		seasonService := service.NewSeasonService(db)
-		gameService := service.NewGameService(db)
-		gameStateService := service.NewGameStateService(gameStateClient)
-		v1public.Use(middleware.CompetitionStructureValidator(logger, seasonService, gameService))
+		seasonService := service.NewSeasonService(cfg.DB)
+		gameService := service.NewGameService(cfg.DB)
+		gameStateService := service.NewGameStateService(cfg.GameStateClient)
+		v1public.Use(middleware.CompetitionStructureValidator(cfg.Logger, seasonService, gameService))
 
 		// competitions
-		competitionService := service.NewCompetitionService(db)
-		v1public.POST("/competitions", handleCreateCompetition(logger, validate, competitionService))
-		v1public.GET("/competitions", handleGetCompetitions(logger, validate, competitionService))
-		v1public.GET("/competitions/:competitionID", handleGetCompetition(logger, competitionService))
-		v1public.PUT("/competitions/:competitionID", handleUpdateCompetition(logger, validate, competitionService))
-		v1public.DELETE("/competitions/:competitionID", handleDeleteCompetition(logger, competitionService))
+		competitionService := service.NewCompetitionService(cfg.DB)
+		v1public.POST("/competitions", handleCreateCompetition(cfg.Logger, cfg.Validate, competitionService))
+		v1public.GET("/competitions", handleGetCompetitions(cfg.Logger, cfg.Validate, competitionService))
+		v1public.GET("/competitions/:competitionID", handleGetCompetition(cfg.Logger, competitionService))
+		v1public.PUT("/competitions/:competitionID", handleUpdateCompetition(cfg.Logger, cfg.Validate, competitionService))
+		v1public.DELETE("/competitions/:competitionID", handleDeleteCompetition(cfg.Logger, competitionService))
 
 		// seasons
-		v1public.POST("/competitions/:competitionID/seasons", handleCreateSeason(logger, validate, seasonService))
-		v1public.GET("/competitions/:competitionID/seasons", handleGetSeasons(logger, validate, seasonService))
-		v1public.GET("/competitions/:competitionID/seasons/:seasonID", handleGetSeason(logger))
-		v1public.PUT("/competitions/:competitionID/seasons/:seasonID", handleUpdateSeason(logger, validate, seasonService))
-		v1public.DELETE("/competitions/:competitionID/seasons/:seasonID", handleDeleteSeason(logger, seasonService))
+		v1public.POST("/competitions/:competitionID/seasons", handleCreateSeason(cfg.Logger, cfg.Validate, seasonService))
+		v1public.GET("/competitions/:competitionID/seasons", handleGetSeasons(cfg.Logger, cfg.Validate, seasonService))
+		v1public.GET("/competitions/:competitionID/seasons/:seasonID", handleGetSeason(cfg.Logger))
+		v1public.PUT("/competitions/:competitionID/seasons/:seasonID", handleUpdateSeason(cfg.Logger, cfg.Validate, seasonService))
+		v1public.DELETE("/competitions/:competitionID/seasons/:seasonID", handleDeleteSeason(cfg.Logger, seasonService))
 
 		// games
-		v1public.POST("/competitions/:competitionID/seasons/:seasonID/games", handleCreateGame(logger, validate, gameService))
-		v1public.GET("/competitions/:competitionID/seasons/:seasonID/stages/:stageID/games", handleGetGames(logger, gameService))
-		v1public.GET("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleGetGame(logger, gameService))
-		v1public.PUT("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleUpdateGame(logger, gameService, validate))
-		v1public.DELETE("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleDeleteGame(logger, gameService))
-		v1public.GET("/competitions/:competitionID/seasons/:seasonID/games/:gameID/live", handleWatchGame(logger, gameStateService))
+		v1public.POST("/competitions/:competitionID/seasons/:seasonID/games", handleCreateGame(cfg.Logger, cfg.Validate, gameService))
+		v1public.GET("/competitions/:competitionID/seasons/:seasonID/stages/:stageID/games", handleGetGames(cfg.Logger, gameService))
+		v1public.GET("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleGetGame(cfg.Logger, gameService))
+		v1public.PUT("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleUpdateGame(cfg.Logger, gameService, cfg.Validate))
+		v1public.DELETE("/competitions/:competitionID/seasons/:seasonID/games/:gameID", handleDeleteGame(cfg.Logger, gameService))
+		v1public.GET("/competitions/:competitionID/seasons/:seasonID/games/:gameID/live", handleWatchGame(cfg.Logger, gameStateService))
 
 		// teams
-		teamService := service.NewTeamService(db)
-		v1public.POST("/teams", handleCreateTeam(logger, validate, teamService))
-		v1public.GET("/teams", handleGetTeams(logger, validate, teamService))
-		v1public.GET("/teams/:teamID", handleGetTeam(logger, teamService))
-		v1public.PUT("/teams/:teamID", handleUpdateTeam(logger, validate, teamService))
-		v1public.DELETE("/teams/:teamID", handleDeleteTeam(logger, teamService))
+		teamService := service.NewTeamService(cfg.DB)
+		v1public.POST("/teams", handleCreateTeam(cfg.Logger, cfg.Validate, teamService))
+		v1public.GET("/teams", handleGetTeams(cfg.Logger, cfg.Validate, teamService))
+		v1public.GET("/teams/:teamID", handleGetTeam(cfg.Logger, teamService))
+		v1public.PUT("/teams/:teamID", handleUpdateTeam(cfg.Logger, cfg.Validate, teamService))
+		v1public.DELETE("/teams/:teamID", handleDeleteTeam(cfg.Logger, teamService))
 	}
 
 	return router
@@ -79,7 +86,6 @@ func SetupRouter(db db_handler.DB, logger zerolog.Logger, validate *validator.Va
 
 func healthCheck(db db_handler.DB, logger zerolog.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
 		if err := db.HealthCheck(); err != nil {
 			logger.Error().
 				Err(err).
