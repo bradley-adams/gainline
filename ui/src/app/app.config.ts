1a16b8287539ff21ca72848e938dda1c22a913b3
Bradley Adams <bradley.wayne.adams@gmail.com>
2026-08-16 08:10:29 +1200

    Add Auth0 Angular SDK and configure providers

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -2,23 +2,25 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { ApplicationConfig } from '@angular/core'
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideRouter } from '@angular/router'
-import { provideAuth0 } from '@auth0/auth0-angular'
+import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular'
 
 import { environment } from '../environments/environment'
 import { routes } from './app.routes'
-import { authInterceptor } from './interceptors/auth.interceptor'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideAnimations(),
-        provideHttpClient(withInterceptors([authInterceptor])),
+        provideHttpClient(withInterceptors([authHttpInterceptorFn])),
         provideAuth0({
             domain: environment.auth0Domain,
             clientId: environment.auth0ClientId,
             authorizationParams: {
                 redirect_uri: window.location.origin,
                 audience: environment.auth0Audience
+            },
+            httpInterceptor: {
+                allowedList: [`${environment.apiUrl}/*`]
             }
         })
     ]
