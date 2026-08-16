49f030fd3a646cad17fbd2cccea41acef4930597
Bradley Adams <bradley.wayne.adams@gmail.com>
2026-08-17 06:03:27 +1200

    Protect admin routes with Auth0 guard

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router'
+import { AuthGuard } from '@auth0/auth0-angular'
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component'
 import { CompetitionDetailComponent } from './pages/competition-detail/competition-detail.component'
 import { CompetitionListComponent } from './pages/competition-list/competition-list.component'
@@ -11,32 +12,63 @@ import { SeasonListComponent } from './pages/season-list/season-list.component'
 import { TeamDetailComponent } from './pages/team-detail/team-detail.component'
 import { TeamListComponent } from './pages/team-list/team-list.component'
 
-// prettier-ignore
 export const routes: Routes = [
     { path: '', redirectTo: '/schedule', pathMatch: 'full' },
 
-    { path: 'schedule', component: ScheduleComponent },
-    { path: 'schedule/competitions/:competition-id/seasons/:season-id/games/:game-id', component: ScheduleGameComponent },
+    { path: 'schedule', component: ScheduleComponent, canActivate: [AuthGuard] },
+    {
+        path: 'schedule/competitions/:competition-id/seasons/:season-id/games/:game-id',
+        component: ScheduleGameComponent,
+        canActivate: [AuthGuard]
+    },
 
-    { path: 'admin', component: AdminDashboardComponent },
-    
+    { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] },
+
     // Admin - Competitions
-    { path: 'admin/competitions', component: CompetitionListComponent },
-    { path: 'admin/competitions/create', component: CompetitionDetailComponent },
-    { path: 'admin/competitions/:competition-id', component: CompetitionDetailComponent },
+    { path: 'admin/competitions', component: CompetitionListComponent, canActivate: [AuthGuard] },
+    { path: 'admin/competitions/create', component: CompetitionDetailComponent, canActivate: [AuthGuard] },
+    {
+        path: 'admin/competitions/:competition-id',
+        component: CompetitionDetailComponent,
+        canActivate: [AuthGuard]
+    },
 
     // Admin - Seasons
-    { path: 'admin/competitions/:competition-id/seasons', component: SeasonListComponent },
-    { path: 'admin/competitions/:competition-id/seasons/create', component: SeasonDetailComponent },
-    { path: 'admin/competitions/:competition-id/seasons/:season-id', component: SeasonDetailComponent },
+    {
+        path: 'admin/competitions/:competition-id/seasons',
+        component: SeasonListComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'admin/competitions/:competition-id/seasons/create',
+        component: SeasonDetailComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'admin/competitions/:competition-id/seasons/:season-id',
+        component: SeasonDetailComponent,
+        canActivate: [AuthGuard]
+    },
 
     // Admin - Games
-    { path: 'admin/competitions/:competition-id/seasons/:season-id/games', component: GameListComponent },
-    { path: 'admin/competitions/:competition-id/seasons/:season-id/games/create', component: GameDetailComponent },
-    { path: 'admin/competitions/:competition-id/seasons/:season-id/games/:game-id', component: GameDetailComponent },
+    {
+        path: 'admin/competitions/:competition-id/seasons/:season-id/games',
+        component: GameListComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'admin/competitions/:competition-id/seasons/:season-id/games/create',
+        component: GameDetailComponent,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'admin/competitions/:competition-id/seasons/:season-id/games/:game-id',
+        component: GameDetailComponent,
+        canActivate: [AuthGuard]
+    },
 
     // Admin - Teams
-    { path: 'admin/teams', component: TeamListComponent },
-    { path: 'admin/teams/create', component: TeamDetailComponent },
-    { path: 'admin/teams/:team-id', component: TeamDetailComponent },
+    { path: 'admin/teams', component: TeamListComponent, canActivate: [AuthGuard] },
+    { path: 'admin/teams/create', component: TeamDetailComponent, canActivate: [AuthGuard] },
+    { path: 'admin/teams/:team-id', component: TeamDetailComponent, canActivate: [AuthGuard] }
 ]
